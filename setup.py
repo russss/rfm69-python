bf171a2964e5f671f2fd66a177a5822f8bf69fa2
Russ Garrett <russ@garrett.co.uk>
2016-09-04 10:22:41 +0100

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 from setuptools import find_packages, setup
 
 setup(name="rfm69",
-            version="0.1",
-            description="Library for accessing the HopeRF RFM69-series radio modules via GPIO/SPI.",
-            author="Russ Garrett",
-            author_email='russ@garrett.co.uk',
-            platforms=["any"],
-            license="MIT",
-            url="http://github.com/russss/rfm69-python",
-            packages=find_packages(),
-            install_requires=[
-                'enum >= 0.4.6',
-                'RPi.GPIO >= 0.6.0',
-                'spidev >= 3.0'
-            ]
-            )
+      version="0.2",
+      description="Library for accessing the HopeRF RFM69-series radio modules via GPIO/SPI.",
+      author="Russ Garrett",
+      author_email='russ@garrett.co.uk',
+      platforms=["any"],
+      license="MIT",
+      url="http://github.com/russss/rfm69-python",
+      packages=find_packages(),
+      install_requires=[
+          'enum >= 0.4.6',
+          'RPi.GPIO >= 0.6.0',
+          'spidev >= 3.0'
+      ]
+      )
